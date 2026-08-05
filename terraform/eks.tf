38225f7ab2109fcf01f5371943f1c73459746c38
Jung-Ilwoong <jiw3052@naver.com>
2026-08-05 16:25:00 +0900

    Fix EKS version/access-entry and ECR force_delete bugs found during first real apply

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -3,12 +3,14 @@ module "eks" {
   version = "~> 20.0"
 
   cluster_name    = "${var.project_name}-eks"
-  cluster_version = "1.30"
+  cluster_version = "1.34"
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
   cluster_endpoint_public_access = true
+
+  enable_cluster_creator_admin_permissions = true
 
   eks_managed_node_groups = {
     default = {
